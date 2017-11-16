ec214513c2dad167acdc6a8bcaae6d91e2da6906
洪栋城 <thank_you1996@outlook.com>
2017-11-16 10:55:36 +0800

    1.ConvertClass   添加位运算分类 添加按位取反方法

---
+++ CommonMethodTests1/ConvertClassTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonMethod.Tests
+{
+    [TestClass()]
+    public class ConvertClassTests
+    {
+        [TestMethod()]
+        public void BitOperation_BitwiseNOTTest()
+        {
+            UInt32 intValue = 1;
+            UInt32 intResult = ConvertClass.BitOperation_BitwiseNOT(intValue);
+            Assert.AreEqual(intResult, 1);
+        }
+    }
+}
