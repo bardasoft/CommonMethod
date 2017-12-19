cc278b58bbbb3e5756db66e52c895830d6430b60
洪栋城 <1296214557@qq.com>
2017-12-19 10:20:02 +0800

    1.Common_Web    添加接口 获取电话号码归属地

--- CommonMethod/Common_Web.cs
+++ CommonMethod/Common_Web.cs
@@ -43,6 +43,40 @@ namespace CommonMethod
         }
 
         /// <summary>
+        /// HttpPost请求
+        /// </summary>
+        /// <param name="strUrl"></param>
+        /// <param name="strPostData"></param>
+        /// <param name="strSteamWriteEncoding"></param>
+        /// <param name="strStreamReadEncoding"></param>
+        /// <returns></returns>
+        public static string HttpPost(string strUrl, string strPostData, string strSteamWriteEncoding, string strStreamReadEncoding)
+        {
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strUrl);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = Encoding.UTF8.GetByteCount(strPostData);
+            //request.CookieContainer = cookie;
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding(strSteamWriteEncoding));
+            myStreamWriter.Write(strPostData);
+            myStreamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //response.Cookies = cookie.GetCookies(response.ResponseUri);
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(strStreamReadEncoding));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
+
+
+        /// <summary>
         /// HttpGet请求
         /// </summary>
         /// <param name="Url"></param>
@@ -60,11 +94,37 @@ namespace CommonMethod
             string retString = myStreamReader.ReadToEnd();
             myStreamReader.Close();
             myResponseStream.Close();
-
             return retString;
         }
 
 
+        /// <summary>
+        /// HttpGet请求
+        /// </summary>
+        /// <param name="strUrl"></param>
+        /// <param name="strGetDataStr"></param>
+        /// <param name="strSteamWriteEncoding"></param>
+        /// <param name="strStreamReadEncoding"></param>
+        /// <returns></returns>
+        public static string HttpGet(string strUrl, string strGetDataStr, string strSteamWriteEncoding, string strStreamReadEncoding)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strUrl + (strGetDataStr == "" ? "" : "?") + strGetDataStr);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(strStreamReadEncoding));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
+
+        /// <summary>
+        /// 获取网络时间
+        /// </summary>
+        /// <returns></returns>
         public static string GetNetDataTime()
         {
             WebRequest request = null;
@@ -93,5 +153,57 @@ namespace CommonMethod
                 { headerCollection.Clear(); }
             }
         }
+
+        public static string QueryPhoneAttribution(string strPhome)
+        {
+            StringBuilder sbJsonData = new StringBuilder();
+            string strProv = "";
+            string strCity = "";
+            string strType = "";
+            bool bolResult = false;
+            string strUrl = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?";
+            string strData ="cb=jQuery1102033239549674370017_1513645089824&resource_name=guishudi&query="+ strPhome + "&_=1513645089826";
+            int Temp_intIndex1=0;
+            int Temp_intIndex2= 0;
+            int Temp_intLength = 0;
+            try
+            {
+                string Temp_strRequestResult = HttpPost(strUrl, strData, "gb2312", "gb2312");
+                //不在单独引用 JSON DLL 独自进行字符串解析
+                if (!string.IsNullOrEmpty(Temp_strRequestResult))
+                {
+                    //省
+                    Temp_intIndex1 = Temp_strRequestResult.IndexOf("\"prov\":") + 8;
+                    Temp_intIndex2 = Temp_strRequestResult.IndexOf("\"city\":") - 3;
+                    Temp_intLength = Temp_intIndex2 - Temp_intIndex1;
+                    strProv = Temp_strRequestResult.Substring(Temp_intIndex1, Temp_intLength);
+
+                    //市
+                    Temp_intIndex1 = Temp_strRequestResult.IndexOf("\"city\":") + 8;
+                    Temp_intIndex2 = Temp_strRequestResult.IndexOf("\"type\":") - 3;
+                    Temp_intLength = Temp_intIndex2 - Temp_intIndex1;
+                    strCity = Temp_strRequestResult.Substring(Temp_intIndex1, Temp_intLength);
+
+                    //运营商
+                    Temp_intIndex1 = Temp_strRequestResult.IndexOf("\"type\":") + 8;
+                    Temp_intIndex2 = Temp_strRequestResult.IndexOf("\"SiteId\":") - 3;
+                    Temp_intLength = Temp_intIndex2 - Temp_intIndex1;
+                    strType = Temp_strRequestResult.Substring(Temp_intIndex1, Temp_intLength);
+                    bolResult = true;
+                }
+            }
+            catch
+            {
+                bolResult = false;
+            }
+            sbJsonData.Append("[{");
+            sbJsonData.Append("\"result\":\"" + bolResult.ToString().ToLower() + "\",");
+            sbJsonData.Append("\"prov\":\"" + strProv + "\",");
+            sbJsonData.Append("\"city\":\"" + strCity + "\",");
+            sbJsonData.Append("\"type\":\"" + strType + "\"");
+            sbJsonData.Append("}]");
+            return sbJsonData.ToString() ;
+        }
+
     }
 }
--- CommonMethodTests1/Common_WebTests.cs
+++ CommonMethodTests1/Common_WebTests.cs
@@ -16,6 +16,7 @@ namespace CommonMethod.Tests
         {
             string strUrl = "https://open.ys7.com/api/lapp/token/get";
             string strData = "appKey=1acd8ddc451f48a4b8b4666716e8f9ce&appSecret=518335cd3421f16a4b4e88164225c432";
+
             string strResulr = Common_Web.HttpPost(strUrl, strData);
             Assert.AreEqual(strResulr, 1);
         }
@@ -26,6 +27,31 @@ namespace CommonMethod.Tests
             string strResult = Common_Web.GetNetDataTime();
             DateTime tim = Convert.ToDateTime(strResult);
             Assert.AreEqual(tim, "1");
+        }
+
+        [TestMethod()]
+        public void HttpGetTest()
+        {
+            string strUrl = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?";
+            string strData = "cb=jQuery1102033239549674370017_1513645089824&resource_name=guishudi&query=15159772128&_=1513645089826";
+            string strResulr = Common_Web.HttpGet(strUrl, strData);
+            Assert.AreEqual(strResulr, "1");
+        }
+
+        [TestMethod()]
+        public void HttpPostTest1()
+        {
+            string strUrl = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?";
+            string strData = "cb=jQuery1102033239549674370017_1513645089824&resource_name=guishudi&query=151597722128&_=1513645089826";
+            string strResulr = Common_Web.HttpPost(strUrl, strData, "gb2312", "gb2312");
+            Assert.AreEqual(strResulr, "1");
+        }
+
+        [TestMethod()]
+        public void QueryPhoneAttributionTest()
+        {
+            string strResult = Common_Web.QueryPhoneAttribution("15159772128");
+            Assert.AreEqual(strResult, "1");
         }
     }
 }
