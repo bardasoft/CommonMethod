9a1e41489c774a4b04ede32e712616eca5254f4f
洪栋城 <1296214557@qq.com>
2018-06-29 19:07:45 +0800

    Xml操作类完善

--- CommonMethod/XmlUse.cs
+++ CommonMethod/XmlUse.cs
@@ -18,6 +18,9 @@ namespace CommonMethod
         }
 
 
+
+
+        #region 数据更新
         /// <summary>
         /// 添加节点
         /// </summary>
@@ -27,7 +30,7 @@ namespace CommonMethod
         /// <param name="strsField"></param>
         /// <param name="strsValue"></param>
         /// <returns></returns>
-        public static bool AddNodeInfo(string strFilePath,string strParentName,string strNodeName,string[] strsField,string[] strsValue)
+        public static bool AddNodeInfo(string strFilePath, string strParentName, string strNodeName, string[] strsField, string[] strsValue)
         {
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load(strFilePath);
@@ -43,7 +46,7 @@ namespace CommonMethod
         }
 
 
-        public static bool UpdateNodeInfo(string strFilePath, string strParentName, string strNodeName, string[] strsField, string[] strsValue,bool bolAutoAdd=false)
+        public static bool UpdateNodeInfo(string strFilePath, string strParentName, string strNodeName, string[] strsField, string[] strsValue, bool bolAutoAdd = false)
         {
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load(strFilePath);
@@ -89,6 +92,76 @@ namespace CommonMethod
             return true;
         }
 
+        public static bool UpdateNodeInfo(string strFilePath, string strParentName, XmlNode xn)
+        {
+            bool bolResult = false;
+            XmlElement element;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(strFilePath);
+            XmlNode nodeParent = xmlDoc.SelectSingleNode(strParentName);   //父节点
+            XmlNode node = nodeParent.SelectSingleNode(xn.Name);
+            if (node == null)
+            {
+                nodeParent.AppendChild(xn);
+            }
+            else
+            {
+                foreach (XmlAttribute attr in xn.Attributes)
+                {
+                    XmlAttribute Temp_attr = node.Attributes[attr.Name];
+                    if (Temp_attr == null )
+                    {
+                        element = (XmlElement)node;
+                        element.SetAttribute(attr.Name, attr.Value);
+                    }
+                    else
+                    {
+                        Temp_attr.Value = attr.Value;
+                    }
+                }
+            }
+            xmlDoc.Save(strFilePath);
+
+            return bolResult;
+        }
+
+        public static bool UpdateNodeInfo(string strFilePath, string strParentName, XmlElement xe)
+        {
+            bool bolResult = false;
+            XmlElement element;
+            //XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.Load(strFilePath);
+            XmlDocument xmlDoc = xe.OwnerDocument;
+            XmlNode nodeParent = xmlDoc.SelectSingleNode(strParentName);   //父节点
+            XmlNode node = nodeParent.SelectSingleNode(xe.Name);
+            if (node == null)
+            {
+                nodeParent.AppendChild(xe);
+            }
+            else
+            {
+                foreach (XmlAttribute attr in xe.Attributes)
+                {
+                    XmlAttribute Temp_attr = node.Attributes[attr.Name];
+                    if (Temp_attr == null)
+                    {
+                        element = (XmlElement)node;
+                        element.SetAttribute(attr.Name, attr.Value);
+                    }
+                    else
+                    {
+                        Temp_attr.Value = attr.Value;
+                    }
+                }
+            }
+            xmlDoc.Save(strFilePath);
+
+            return bolResult;
+        }
+        #endregion
+
+        #region 数据获取
+
         /// <summary>
         /// 获取节点信息
         /// </summary>
@@ -97,9 +170,9 @@ namespace CommonMethod
         /// <param name="strNodeName"></param>
         /// <param name="strField"></param>
         /// <returns></returns>
-        public static string  GetNodeInfo(string strFilePath, string strParentName,string strNodeName, string strField)
+        public static string GetNodeInfo(string strFilePath, string strParentName, string strNodeName, string strField)
         {
-            string  strResult = "";
+            string strResult = "";
             try
             {
                 XmlDocument xmlDoc = new XmlDocument();
@@ -114,6 +187,19 @@ namespace CommonMethod
             }
             return strResult;
         }
-        
+
+        public static XmlNode GetNodeInfo(string strFilePath, string strParentName, string strNodeName)
+        {
+            XmlNode result = null;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(strFilePath);
+            XmlNode nodeParent = xmlDoc.SelectSingleNode(strParentName);   //父节点
+            result = nodeParent.SelectSingleNode(strNodeName);
+            return result;
+        }
+
+        #endregion
+
+
     }
 }
--- CommonMethodTests/XmlUseTests.cs
+++ CommonMethodTests/XmlUseTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace CommonMethod.Tests
 {
@@ -51,7 +52,7 @@ namespace CommonMethod.Tests
             string strNodeName = "SetingInfo1";
             string strField = "key1";
             string strValue = "123";
-            bool bolResult = XmlUse.UpdateNodeInfo(strFilePath, strParertName, strNodeName, strField, strValue,true);
+            bool bolResult = XmlUse.UpdateNodeInfo(strFilePath, strParertName, strNodeName, strField, strValue, true);
             Assert.IsTrue(bolResult);
         }
 
@@ -64,6 +65,47 @@ namespace CommonMethod.Tests
             string[] strsField = new string[] { "key", "value", "defaultvalue" };
             string[] strsValue = new string[] { "123", "123", "123" };
             bool bolResult = XmlUse.UpdateNodeInfo(strFilePath, strParertName, strNodeName, strsField, strsValue);
+            Assert.IsTrue(bolResult);
+        }
+
+        [TestMethod()]
+        public void GetNodeIofoTest()
+        {
+            string strFilePath = Environment.CurrentDirectory + "\\LayoutSetting.xml";
+            string strParertName = "LayoutSetting";
+            string strNodeName = "SetingInfo123123";
+            string[] strsField = new string[] { "key", "value", "defaultvalue" };
+            string[] strsValue = new string[] { "123", "123", "123" };
+            XmlNode node = XmlUse.GetNodeInfo(strFilePath, strParertName, strNodeName);
+            Assert.AreEqual(node.Attributes["key"].Value, "1");
+        }
+
+        [TestMethod()]
+        public void UpdateNodeInfoTest2()
+        {
+            string strFilePath = Environment.CurrentDirectory + "\\LayoutSetting.xml";
+            string strParertName = "LayoutSetting";
+            string strNodeName = "SetingInfo";
+            XmlNode node = XmlUse.GetNodeInfo(strFilePath, strParertName, strNodeName);
+            //node.Attributes.Append(new XmlAttribute)
+            XmlElement element = (XmlElement)node;
+            element.SetAttribute("TestAttr", "TestAttrValue");
+            bool bolResult = XmlUse.UpdateNodeInfo(strFilePath, strParertName, node);
+            Assert.IsTrue(bolResult);
+
+        }
+
+        [TestMethod()]
+        public void UpdateNodeInfoTest3()
+        {
+            string strFilePath = Environment.CurrentDirectory + "\\LayoutSetting.xml";
+            string strParertName = "LayoutSetting";
+            string strNodeName = "SetingInfo";
+            XmlNode node = XmlUse.GetNodeInfo(strFilePath, strParertName, strNodeName);
+            //node.Attributes.Append(new XmlAttribute)
+            XmlElement element = (XmlElement)node;
+            element.SetAttribute("TestAttr111", "TestAttrValue");
+            bool bolResult = XmlUse.UpdateNodeInfo(strFilePath, strParertName, element);
             Assert.IsTrue(bolResult);
         }
     }
